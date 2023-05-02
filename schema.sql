07adc80d5e0f00b20547cd60a16a906c1ff41e43
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-02 14:02:58 +0300

    update all files with SQL commands

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+    id int primary key not null,
+    name varchar(200) not null,
+    date_of_birth date not null,
+    escape_attempts int not null,
+    neutered bool not null,
+    weight_kg decimal not null
+);
